6bfb6deb9c391caa09bde1b8c9bee53c74158028
Rodrigo Medina <rmedina@technisys.com>
2019-02-01 14:05:06 -0300

    se toma la dimension del array para la condicion

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -61,11 +61,11 @@ public class FileOpener2 extends CordovaPlugin {
 			String fileUrl = args.getString(0);
 			String contentType = args.getString(1);
 			Boolean openWithDefault = false;
-			if(args.length() > 2){
+			if(args.toList().size() > 2){
 				openWithDefault = args.getBoolean(2);
 			}
                         String message = "Abrir con...";
-                        if(args.length() > 3){
+                        if(args.toList().size() > 3){
                              message = args.getString(3);
                         }
 			this._open(fileUrl, contentType, openWithDefault, message, callbackContext);
